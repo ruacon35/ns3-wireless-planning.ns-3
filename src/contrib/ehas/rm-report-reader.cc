02a411289f033cf3c11a3da318fd8c551d5f0e43
arnau <arnau@localhost>
2010-02-24 13:54:39 +0100

    Use simplified RMW report to create netData

--- src/contrib/ehas/rm-report-reader.cc
+++ src/contrib/ehas/rm-report-reader.cc
@@ -27,450 +27,109 @@ NS_LOG_COMPONENT_DEFINE ("rm-report-reader");
 
 using namespace std;
 
-namespace ns3
+namespace ns3 {
+
+RmReportReader::RmReportReader () { }
+
+RmReportReader::~RmReportReader () { }
+
+TypeId
+RmReportReader::GetTypeId (void)
 {
-
- RmReportReader::RmReportReader ()
- {
-  // Some general lines
-  m_errorMsg = "Unrecognized report structure.";
-  m_dashLine = "---------------------------------------------------------------------------";
-  m_blankLine = "";
-  m_doubleSpace = "  ";
-  m_space = " ";
- }
-
- RmReportReader::~RmReportReader () { }
-
- TypeId
- RmReportReader::GetTypeId (void)
- {
   static TypeId tid = TypeId ("ns3::RmReportReader")
-          .SetParent<Object > ()
-          ;
+    .SetParent<Object > ()
+    ;
   return tid;
- }
+}
 
- uint16_t
- RmReportReader::Section2vector (ifstream &file, string begining, string header, vector<string> &section)
- {
+vector<string> 
+RmReportReader::split (const string &s, char delim) 
+{
+  vector<string> elems;
+  stringstream ss(s);
+  string item;
+  while(getline(ss, item, delim)) 
+    {
+      elems.push_back(item);
+    }
+  return elems;
+}
+
+vector<string>
+RmReportReader::GetSection (vector<string> &lines, string start, string end)
+{
+  vector<string> output_lines;
   string line;
+  bool inSection = false;
 
-  /* Structure:
-   * m_dashLine
-   * sectionBeginig
-   * m_dashLine
-   * sectionHeader
-   * m_blankLine
-   * data
-   *
-   * e.g.:
-   * -----------
-   * section
-   * -----------
-   * header
-   *
-   * data
-   */
-
-  while (line.compare (begining) != 0)
+  for (uint32_t i = 0; i < lines.size(); i++) 
    {
-    getline (file, line); //advance in the file
+     line = lines[i];
+     if (!inSection && line == start) { inSection = true; }                
+     else if (inSection && line.find (end) == 0) { break; }
+     else if (inSection && line != "") { output_lines.push_back (line); }
    }
-  getline (file, line);
-  if (line.compare (m_dashLine) == 0)
-   {
-    if (header.compare (m_blankLine) != 0)
-     {//Has header
-      getline (file, line);
-      if (line.compare (header) == 0)
-       {
-        getline (file, line);
-        if (line.compare (m_blankLine) == 0)
-         {
-          getline (file, line);
-          while (line.compare (m_dashLine) != 0 && file.eof () != 1)// end of section
-           {
-            section.push_back (line);
-            NS_LOG_DEBUG ("sec: " << line);
-            getline (file, line);
-           }
-         }
-        else // m_blankLine
-         {
-          NS_LOG_DEBUG (m_errorMsg << " 2 ");
-          return -2;
-         }
-       }
-     }
-    else // no header
-     {
-      getline (file, line);
-      while (line.compare (m_dashLine) != 0 && file.eof () != 1)// end of section
-       {
-        section.push_back (line);
-        NS_LOG_DEBUG ("sec no header: " << line);
-        getline (file, line);
-       }
-     }// no header
-   }
-  else // m_dashLine
-   {
-    NS_LOG_DEBUG (m_errorMsg << " 1 ");
-    return -1;
-   }
+  return output_lines;
+}
 
-  return 0;
- }
+vector<string>
+RmReportReader::GetSubSectionNames (vector<string> &lines, string header)
+{
+  vector<string> names;
+  for (uint32_t i = 0; i < lines.size(); i++)
+    {
+      string line = lines[i];
+      if (line.find (header) == 0)
+        {
+          string current_net = string (line, header.size(), line.size() - header.size());
+          names.push_back (current_net);
+        }
+    } 
+  return (names);
+}
 
- uint16_t
- RmReportReader::GetNodeNames (vector<string> sectionInfo, vector<string> &sectionNames)
- {
-  // Separation character combination used: DOUBLE SPACE
-  // max size 20 chars
-  // http://www.bgsu.edu/departments/compsci/docs/string.html
-  string elementInfo;
-  string name;
-  string::size_type name_max_length = 20;
-
-  for (uint16_t i = 0; i < sectionInfo.size (); i++)
-   {
-    elementInfo = sectionInfo.at (i);
-    name = RmReportReader::GetName (elementInfo, name_max_length);
-    sectionNames.push_back (name);
-   }
-  return 0;
- }
-
- string
- RmReportReader::GetName (string line, string::size_type max_length)
- {
-  string name;
-
-  NS_LOG_DEBUG ("Line: #" << line << "#");
-  string::size_type found = line.find (m_doubleSpace); //double space
-  NS_LOG_DEBUG ("double space found: " << found);
-  if (found > max_length)
-   {
-    found = max_length;
-    NS_LOG_DEBUG ("  Found reduced: " << found);
-   }
-  name = line.substr (0, found);
-  NS_LOG_DEBUG ("Name limited: #" << name << "#");
-
-  //Cleaning spaces from the name, at the begining and at the end.
-  found = line.find (m_space);
-  if (found == 0)
-   {
-    NS_LOG_DEBUG ("Begining: space found: " << found);
-    name = line.substr (1, name.size () - 1);
-    NS_LOG_DEBUG ("Name without first space: #" << name << "#");
-   }
-  /*
-    //must verify if the last char is an space: e.g. #Laykatuyoc Troncal #
-    // Does it affect on something?
-    string lastChar = line.at (line.size () - 1);
-    NS_LOG_DEBUG ("End: space found: " << found << " last char #" << lastChar << "#");
-    if (m_space.compare (lastChar))
-     {
-      NS_LOG_DEBUG ("End: space found: " << found);
-      name = line.substr (0, line.size () -1);
-   * NS_LOG_DEBUG ("Name without last space: #" << name << "#");
-   */
-  NS_LOG_DEBUG ("Name: #" << name << "#"); // delimiters to distiguish spaces
-
-  return name;
- }
-
- uint16_t
- RmReportReader::GetNets (vector<string> netsInfo, vector< vector<string> > &netInfoMatrix)
- {
-  // blank line (at the begining)
-  // Number. Name
-  // 7 lines
-  // blank line
-  // "Net members:               #  01 02 ... 0n Role:         System:             Antenna:"
-  // Member names (n)                (matrix)    Terminal/Node  (system)            high? m
-  // blank line
-  // "                           Quality = 50 - number of resend"
-  // blank line (at the end)
-
-  string eon = "                           Quality = 50 - number of resend"; // End of Net
-  uint16_t j = 0; // for debbuging
-  for (uint16_t i = 0; i < netsInfo.size (); i++)
-   {
-    vector<string> netInfo;
-    i++; // Blank Line at the begining
-    while (eon.compare (netsInfo.at (i)) != 0)
-     {
-      NS_LOG_DEBUG ("net info: " << i << "," << j << " " << netsInfo.at (i));
-      netInfo.push_back (netsInfo.at (i++));
-     }
-    NS_LOG_DEBUG ("net info (out of while): " << i << "," << j << " " << netsInfo.at (i) << endl);
-    netInfo.push_back (netsInfo.at (i)); // we delete last line = blank line
-    i++; // Blank line at the end
-
-    netInfoMatrix.push_back (netInfo);
-    j++;
-   }
-  return 0;
- }
-
- uint16_t
- RmReportReader::GetSubnetData (vector<vector<string> > netInfoMatrix, vector<NetDataStruct::SubnetData> &vSubnetData)
- {
-  /* Generalization:
-   * n. Net name (beeing n the number of net)
-   * Cluster topology
-   * 2400.0 MHz to 2483.0 MHz
-   * Vertical polarization
-   * Mode of variability is Broadcast, at
-   * 50% of time, 50% of locations, 50% of situations
-   * Refractivity= 301 N-units, conductivity= 0.005 S/m, permittivity= 15
-   * Continental temperate climate
-   * blank line
-   * Net members:
-   *  j lines (beeing j the number of nodes of the net)
-   * blank line
-   * string eon = "                           Quality = 50 - number of resend"; // En of Net
-   *
-   *
-   */
-  vector<string> netInfo;
-  string line;
-  string lineOne;
-  string netName;
-  string::size_type found;
-  string::size_type name_max_length = 27;
-
-  for (uint16_t i = 0; i < netInfoMatrix.size (); i++)
-   {
-    NetDataStruct::SubnetData subnetData;
-    netInfo = netInfoMatrix.at (i);
-    vector<string> members;
-    NS_LOG_DEBUG ("init");
-    Print::VectorString (members);
-
-    uint16_t j = 0;
-    line = netInfo.at (j); //Net name
-    ostringstream oss;
-    oss << i + 1 << ".";
-    lineOne = oss.str ();
-    NS_LOG_DEBUG ("~~~~Net number: " << i + 1);
-    NS_LOG_DEBUG (lineOne);
-
-    found = line.find (lineOne);
-
-    NS_LOG_DEBUG ("found point: " << found);
-    netName = line.substr (found + lineOne.size (), line.size ());
-    subnetData.name = RmReportReader::GetName (netName, name_max_length);
-    NS_LOG_DEBUG ("Net name: #" << subnetData.name << "#" << " i: " << i << " j: " << j); // delimiters to distiguish spaces
-    //must verify if the last char is an space: e.g. #Laykatuyoc Troncal #
-
-
-    j += 9; // go to Net members line
-
-    string lineMembers = "Net members:";
-    line = netInfo.at (j++); //Net Members:
-    found = line.find (lineMembers);
-    if (found == 0)// Line 9 = Net members line => known format
-     {
-      while (netInfo.at (j).compare (m_blankLine) != 0)
-       {
-        line = netInfo.at (j++);
-        NS_LOG_DEBUG ("line members: " << line << " i:" << i << " j++:" << j);
-        members.push_back (line);
-       }
-     }
-    else // Net Members
-     {
-      NS_LOG_DEBUG (m_errorMsg << " m1 ");
-      return -1;
-     }
-
-    //members processing
-    GetMembersData (members, subnetData);
-
-    vSubnetData.push_back (subnetData);
-   }
-  return 0;
- }
-
- uint16_t
- RmReportReader::GetMembersData (vector<string> members, NetDataStruct::SubnetData &subnetData)
- {
-  /* e.g.
- Net members:               #  01 02 03 Role:         System:             Antenna:
- LAYKATUYOC                 01    50 00 Node          Laykatuyoc Troncal P10.0m
- ACCHA BRIDGE               02 50       Terminal      Accha Repetidor Clie5.0m
- HUASCAR                    03 00       Terminal      Huascar Troncal PCMC11.0m
-   */
-  string member;
-  string node;
-  string matrix;
-  string role;
-  string system;
-
-  //empirical
-  string::size_type name_max_length = 27;
-  string::size_type matrix_max_length = 3 * (members.size () + 1);
-  string::size_type role_max_length = 14;
-  string::size_type system_max_length = 20;
-
-  uint16_t i_b; // begining index
-  uint16_t i_e; // end index
-  //uint16_t eoName = 26; // End of Name
-
-  // Get substrings
-  for (uint16_t i = 0; i < members.size (); i++)
-   {
-    NS_LOG_DEBUG ("  member: " << i);
-    member = members.at (i);
-    i_e = -1; // init
-
-    i_b = i_e + 1;
-    i_e += name_max_length;
-    node = member.substr (i_b, i_e);
-
-    i_b = i_e + 1;
-    i_e += matrix_max_length;
-    matrix = member.substr (i_b, i_e);
-
-    i_b = i_e + 1;
-    i_e += role_max_length;
-    role = member.substr (i_b, i_e);
-
-    i_b = i_e + 1;
-    i_e += system_max_length;
-    system = member.substr (i_b, i_e);
-
-    subnetData.nodes.push_back (GetName (node, name_max_length));
-    subnetData.roles.push_back (GetName (role, role_max_length));
-    //Matrix not processed
-    subnetData.systems.push_back (GetName (system, system_max_length));
-   }
-  return 0;
- }
-
- NetDataStruct::NetData
- RmReportReader::ReadRmReport (ifstream &file)
- {
-  string line;
-  vector<NetDataStruct::SubnetData> vSubnetData;
+NetDataStruct::NetData
+RmReportReader::ReadRmReport (ifstream &file)
+{ 
   NetDataStruct::NetData netData;
-  NS_LOG_DEBUG ("vSubnetData empty?: " << vSubnetData.empty ());
+  vector<string> report_lines, lines;
+  string line;
+    
+  while (getline(file, line))
+     report_lines.push_back (line);
+     
+  // General information
+  netData.generalInfo = GetSection (report_lines, string("= General information"), string("= "));
 
-  if (file.is_open ())
-   {
-    uint16_t retval;
+  // Nodes
+  lines = GetSection (report_lines, string("= Nodes"), string("= "));
+  for (uint32_t i = 0; i < lines.size(); i++)
+    {
+      netData.nodesInfo.names.push_back(lines[i]);
+    }
 
-    /*
-     * ----------------
-     * Reading Sections
-     * ----------------
-     */
+  // Nets
+  string net_header ("== ");
+  lines = GetSection (report_lines, string("= Nets"), string("= "));
+  vector<string> net_names = GetSubSectionNames (lines, net_header);
 
-
-    /*
-     * General Information
-     */
-
-    string generalBegining = "General information";
-    string generalHeader = "";
-    vector<string> generalInfo;
-    retval = Section2vector (file, generalBegining, generalHeader, generalInfo);
-
-    netData.generalInfo = generalInfo;
-
-    NS_LOG_DEBUG ("end of general info " << retval);
-
-
-    /*
-     * Nodes
-     */
-    string nodesBegining = "Active units information";
-    string nodesHeader = "Name                Location                                     Elevation";
-    vector<string> nodesInfo;
-
-    retval = Section2vector (file, nodesBegining, nodesHeader, nodesInfo);
-
-    NS_LOG_DEBUG ("end of nodes " << retval);
-
-    /*
-     * Systems
-     */
-    string systemsBegining = "Systems";
-    string systemsHeader = "Name                Pwr Tx    Loss  Loss (+)  Rx thr.   Ant. G. Ant. Type";
-    vector<string> systemsInfo;
-
-    retval = Section2vector (file, systemsBegining, systemsHeader, systemsInfo);
-
-    NS_LOG_DEBUG ("end of systems " << retval);
-
-    /*
-     * Nets
-     */
-    string netsBegining = "Active nets information";
-    string netsHeader = "";
-    vector<string> netsInfo;
-
-    retval = Section2vector (file, netsBegining, netsHeader, netsInfo);
-
-    NS_LOG_DEBUG ("end of nets " << retval);
-
-
-    while (!file.eof ())
-     {
-      getline (file, line);
-     }
-    NS_LOG_DEBUG ("End of file reached");
-
-    file.close ();
-
-    /*
-     * -------------
-     * Reading names
-     * -------------
-     */
-
-    /*
-     * Getting node names
-     */
-    vector<string> nodeNames;
-    retval = GetNodeNames (nodesInfo, nodeNames);
-
-    netData.nodesInfo.names = nodeNames;
-
-    /*
-     * Getting system names
-     */
-    vector<string> systemNames;
-    retval = GetNodeNames (systemsInfo, systemNames);
-
-    netData.systemsInfo.names = systemNames;
-
-    /*
-     * Separating nets
-     */
-    vector< vector<string> > netInfoMatrix;
-    retval = GetNets (netsInfo, netInfoMatrix);
-
-    /*
-     * Getting Net parameters
-     */
-
-    retval = GetSubnetData (netInfoMatrix, vSubnetData);
-
-    netData.vSubnetData = vSubnetData;
-
-   }// .is_open
-  else
-   {
-    NS_LOG_DEBUG ("Unable to open file");
-    netData.nodesInfo.names.at (0) = "empty";
-   }
-
+  for (uint32_t i = 0; i < net_names.size(); i++)
+    {
+      NetDataStruct::SubnetData subnetData;
+      string net_name = net_names[i];
+      vector<string> net_lines = GetSection (lines, net_header+net_name, net_header);
+      subnetData.name = net_name;
+      subnetData.mode = split(net_lines[0], ' ')[1];      
+      for (uint32_t j = 2; j < net_lines.size(); j++) 
+        {
+        string net_line = net_lines[j];
+        vector<string> fields = split(net_line, '\t');
+        subnetData.nodes.push_back (fields[0]);
+        subnetData.roles.push_back (fields[1]);
+        }
+      netData.vSubnetData.push_back (subnetData); 
+    }    
   return netData;
- }
+}
 
 } // namespace ns3
--- src/contrib/ehas/rm-report-reader.h
+++ src/contrib/ehas/rm-report-reader.h
@@ -23,67 +23,29 @@ namespace ns3 {
 using namespace std;
 
 /**
- * @brief A class to read Rodio Mobile reports.
+ * @brief A class to read simplified Radio Mobile reports.
  *
  */
 class RmReportReader
 {
 public:
-    /**
-     * Constructor:
-     */
-    RmReportReader();
-    /**
-     * Destructor
-     */
-    ~RmReportReader();
-    
-        static TypeId GetTypeId(void);
+  RmReportReader();
+  ~RmReportReader();
+  static TypeId GetTypeId(void);
 
-    /**
-     * @brief Reads and parses Radio Mobile report.
-     *
-     * This is the main method of the class.
-     *
-     * @param file report.txt
-     *
-     */
-    NetDataStruct::NetData ReadRmReport(ifstream &file);
-
-
-    /**
-     * @brief Divides the different sections of the file into string vectors.
-     *
-     * @param file Radio Mobile report.
-     * @param begining The line that opens the section.
-     * @param header The header of the section.
-     * @param section The string vector where the section will be stored.
-     *
-     * @return 0 if every thing is ok.
-     */
-    uint16_t Section2vector(ifstream &file, string begining, string header, vector<string> &section);
-
-    uint16_t GetNodeNames(vector<string> sectionInfo, vector<string> &sectionNames);
-
-    string GetName(string line, string::size_type max_length);
-
-    uint16_t GetNets(vector<string> netsInfo, vector< vector<string> > &netInfoMatrix);
-
-    uint16_t GetSubnetData(vector <vector<string> > netInfoMatrix, vector<NetDataStruct::SubnetData> &netData);
-
-    uint16_t GetMembersData(vector<string> members, NetDataStruct::SubnetData &netData);
-
-    
-
-    // Some general lines
-    string m_errorMsg;
-    string m_dashLine;
-    string m_blankLine;
-    string m_doubleSpace;
-    string m_space;
-
-    NetDataStruct m_netDataStruct;
-
+  /**
+   * @brief Reads and parses simplified Radio Mobile report.
+   *
+   * This is the main method of the class.
+   *
+   * @param file simple-report.txt
+   *
+   */
+  NetDataStruct::NetData ReadRmReport(ifstream &file);
+private:  
+  vector<string> GetSection (vector<string> &input_lines, string start, string end);
+  vector<string> GetSubSectionNames (vector<string> &input_lines, string header);
+  vector<string> split(const string &s, char delim);
 };
 
 } // namespace ns3
