16c9de1cd29e6f492853b62625a89e4f7bf101a4
arnau <arnau@localhost>
2010-03-15 12:12:31 +0100

    fix wifi/wimax channels assignation

--- scratch/wireless-planning/report-2-config-data.cc
+++ scratch/wireless-planning/report-2-config-data.cc
@@ -51,6 +51,45 @@ namespace ns3 {
     NetworkConfig::NetworkData network;
 
     /*
+     * Channels
+     */
+    NetworkConfig config;
+    NetworkConfig::VectorChannelData vectorChannelData;
+    vector<uint16_t> chIds;
+    uint16_t wifiChId = 0, wimaxChId = 0;
+    
+    for (uint16_t i = 0; i < net.vSubnetData.size (); i++)
+    {
+      NetDataStruct::SubnetData subNetData = net.vSubnetData.at (i);
+      string system = subNetData.systems.at (0); // all nodes of the same net have the same wifi mode
+
+      enum NetworkConfig::CommunicationStandard standard = ReadStandard (subNetData.standard);
+      switch (standard)
+      {
+        case NetworkConfig::WIFI :
+        {
+          string mode = system;
+          chIds.push_back(wifiChId);
+          config.SetWifiChannelData (wifiChId, mode, vectorChannelData.vWifiChData);
+          wifiChId++;
+          break;
+        }
+        case NetworkConfig::WIMAX :
+        {
+          chIds.push_back(wimaxChId);
+          NetworkConfig::WimaxChannelData channel;
+          channel.id = wimaxChId;
+          vectorChannelData.vWimaxChData.push_back (channel);
+          wimaxChId++;
+          break;
+        }
+        default:
+          NS_ASSERT_MSG (0, "No correct communication standard selected");
+      }
+    }
+    network.vectorChannelData = vectorChannelData;
+
+    /*
      * Nodes
      */
     NetworkConfig::VectorNodeData vectorNodeData;
@@ -67,60 +106,29 @@ namespace ns3 {
       node.position = position;
 
       NS_LOG_INFO ("Node: " << i << " " << node.name);
-      uint16_t wifiChId = 1, wimaxChId = 1;
       
       for (uint16_t j = 0; j < net.vSubnetData.size (); j++) // inner loop throw SUBNETS
       {
+        NS_LOG_INFO ("  subnet: " << j);
         NetDataStruct::SubnetData subnet = net.vSubnetData.at (j);
         // Search if the node has a device in each subnet
         int16_t index = Util::FindStrInVectorStr (node.name, subnet.nodes);
         if (index >= 0)// found
         {
-          AddDevice2Node (node, subnet, index, &wifiChId, &wimaxChId);
+          uint8_t chId = chIds.at (j);
+          AddDevice2Node (node, subnet, index, chId+1);
         }
       }
       vectorNodeData.push_back (node); // add node to the tail of the vector.
     }
     network.vectorNodeData = vectorNodeData;
 
-    /*
-     * Channels
-     */
-    NetworkConfig config;
-    NetworkConfig::VectorChannelData vectorChannelData;
-
-    for (uint16_t i = 0; i < net.vSubnetData.size (); i++)
-    {
-      NetDataStruct::SubnetData subNetData = net.vSubnetData.at (i);
-      string system = subNetData.systems.at (0); // all nodes of the same net have the same wifi mode
-
-      enum NetworkConfig::CommunicationStandard standard = ReadStandard (subNetData.standard);
-      switch (standard)
-      {
-        case NetworkConfig::WIFI :
-        {
-          string mode = system;
-          config.SetWifiChannelData (i, mode, vectorChannelData.vWifiChData);
-          break;
-        }
-        case NetworkConfig::WIMAX :
-        {
-          NetworkConfig::WimaxChannelData channel;
-          channel.id = i;
-          vectorChannelData.vWimaxChData.push_back (channel);
-          break;
-        }
-        default:
-          NS_ASSERT_MSG (0, "No correct communication standard selected");
-      }
-    }
-    network.vectorChannelData = vectorChannelData;
 
     return network;
   }
 
   void
-  Report2ConfigData::AddDevice2Node (NetworkConfig::NodeData &node, NetDataStruct::SubnetData subnet, uint16_t index, uint16_t *wifiChId, uint16_t *wimaxChId)
+  Report2ConfigData::AddDevice2Node (NetworkConfig::NodeData &node, NetDataStruct::SubnetData subnet, uint16_t index, uint16_t chId)
   {
     enum NetworkConfig::CommunicationStandard standard = ReadStandard (subnet.standard);
     string role = subnet.roles.at (index);
@@ -132,22 +140,20 @@ namespace ns3 {
     {
       case NetworkConfig::WIFI :
       {
-        NS_LOG_DEBUG ("  adding Wi-Fi device in ch " << *wifiChId);
+        NS_LOG_DEBUG ("  adding Wi-Fi device in ch " << chId);
         NetworkConfig::MacType macType = Role2MacType (role);
-        deviceData = m_config.SetWifiDeviceData (*wifiChId, macType, distance);
-        *wifiChId++;
+        deviceData = m_config.SetWifiDeviceData (chId, macType, distance);
         break;
       }
       case NetworkConfig::WIMAX :
       {
-        NS_LOG_DEBUG ("  adding WiMAX device in ch " << wimaxChId);
+        NS_LOG_DEBUG ("  adding WiMAX device in ch " << chId);
         WimaxHelper::NetDeviceType deviceType = Role2WimaxDeviceType (role);
         string modulation = subnet.systems.at (index);
         WimaxPhy::ModulationType mod = ReadModulation (modulation);
 
-        deviceData = m_config.SetWimaxDeviceData (*wimaxChId, distance, deviceType,
+        deviceData = m_config.SetWimaxDeviceData (chId, distance, deviceType,
                 WimaxHelper::SCHED_TYPE_RTPS, mod);
-        *wimaxChId++;                
         break;
       }
       default:
